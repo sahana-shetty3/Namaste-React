0bb8873f8e086deb054a1aa7ca6e7a033b03e6df
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-11-26 15:38:12 +0530

    updated routers till error page

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
 import Body from "./Body";
-
+import About from "./About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./Contact";
+import Error from "./Error";
 const AppLayout= () =>{
     return(
         <div className="app">
@@ -11,6 +14,21 @@ const AppLayout= () =>{
         </div>
     );
 };
+const appRouter =createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout/>,
+        errorElement:<Error/>
+    },
+    {
+        path :"/about",
+        element:<About/>
+    },
+     {
+        path :"/contact",
+        element:<Contact/>
+    },
+]);
 const root= ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+const Contact =()=>{
+    return(
+        <div>
+            <h1>Contact US</h1>
+            <h2>Welcome to SWIGGY</h2>
+        </div>
+    )
+
+    
+}
+export default Contact
