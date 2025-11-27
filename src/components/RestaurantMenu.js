60cdfdf2049c4436c727e7cf4baa902da45f994b
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-11-27 11:29:09 +0530

    episode 07

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -14,13 +14,25 @@ const RestaurantMenu = ()=>{
        "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9536392&lng=77.695126&restaurantId=581367&catalog_qa=undefined&submitAction=ENTER"
         );
         const json = await data.json();
+       
         console.log(json);
+        setResInfo(json.data);
     };
-    if(resInfo===null)return<Shimmer/>
+
+    if(resInfo===null) return  <Shimmer/>;
+    
+    const{ name,cuisines,costForTwoMessage}=
+    resInfo?.cards[0]?.card?.card?.info;
+
+    const {iteamCards} = resInfo?.cards[2]?.groupedCards?.cardGroupMap?.REGULAR.cards[1]?.card?.card
+    
 
     return (
         <div className="menu">
-            <h1>Name Of the Restaurant</h1>
+            <h1>{name}</h1>
+            <p>
+                {cuisines.join(" ,")}-{costForTwoMessage}</p>
+    
             <h2>Menu</h2>
             <ul>
                 <li>Biriyani</li>
