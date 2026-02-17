806e8cc1f109c2b8eb14b2906649480202526ec5
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-02-17 19:12:54 +0530

    updates

---
+++ src/User.js
@@ -0,0 +1,9 @@
+const User =()=>{
+    return( <div className="user-card">
+        <h2>Name:Sahana</h2>
+        <h2>Location Udupi</h2>
+    </div>
+);
+
+};
+export default User;
---
+++ src/userClass.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserClass extends React.Component{
+    render(){
+        return(
+        <div className="user-card">
+            <h2>Name:SAHANA</h2>
+            <h3>Location :Udupi</h3>
+            </div>
+            )
+            
+        
+    }
+}
+export default UserClass;
