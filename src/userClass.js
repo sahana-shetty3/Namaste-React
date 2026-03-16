03cca7f0ab28d33b579890886146dae0cc1bb75c
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-03-16 16:49:26 +0530

    updates

--- src/userClass.js
+++ src/userClass.js
@@ -3,35 +3,42 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
-
         this.state={
-            count:0,
-            
+            userInfo:{
+                name:"dummy",
+                location:"Default",
+            },
         };
-        console.log("child constructor")
+
+       
+        //console.log("child constructor")
     }
 
-    componentDidMount(){
-        console.log("parent componentDidMount");
+    async componentDidMount(){
+        //console.log("parent componentDidMount");
+        //api call
+
+        const data = await fetch("https://api.github.com/users/sahana-shetty3");
+        const json = await data.json();
+
+        this.setState({
+            userInfo:json,
+        })
+
+        console.log(json);
     }
     
 
     render(){
-        const {location,name}=this.props;
-        const {count} = this.state;
-        console.log("child render")
+        const {location,name}=this.state.userInfo;
+        
+        //console.log("child render")
 
 
         return(
         <div className="user-card">
-            <h1>Count:{count}</h1>
-            <button onClick={
-                ()=>{
-                    this.setState({
-                        count: this.state.count+ 1,
-                    })
-                }
-            }>CountIncrease</button>
+            
+           
          
             <h2>Name:{name}</h2>
             <h3>Location :{location}</h3>
